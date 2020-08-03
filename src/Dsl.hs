6b5f38813003a2cd23b6af813eaa630700e32508
oleg <oleg@Valerias-MacBook-Air.local>
2020-08-03 17:14:57 +0200

    some more tests and filters

--- src/Dsl.hs
+++ src/Dsl.hs
@@ -1,4 +1,24 @@
 module Dsl where
 
+data Item = Item {
+    id :: String,
+    amount :: Int,
+    name :: String,
+    lockedChildren :: [Item],
+    freeChildren :: [Item],
+    basePrice :: Int,
+    discountedBasePrice :: Maybe Int,
+    discountRule :: Maybe String,
+    totalPrice :: Int,
+    discountedTotalPrice :: Maybe Int
+} deriving(Show, Eq)
+
+defaultItem = Item {Dsl.id = "1", amount= 1, name ="PAP_100", lockedChildren =[], freeChildren = [], 
+    basePrice = 100, discountedBasePrice = Nothing, discountRule = Nothing, totalPrice = 10, discountedTotalPrice = Nothing
+}
+
+filterName :: String -> (Item -> Bool)
+filterName fname = \x -> (name x) == fname
+
 absolute :: Int -> Int
 absolute x = x + 1
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,4 +9,11 @@ main = hspec $ do
           
         it "now borke" $
             absolute 3 `shouldBe` 4
+        it "shoudl work with filter" $ do
+            let x = defaultItem {Dsl.id = "1",  name = "aaa"}
+            let y = defaultItem {Dsl.id = "2",  name= "bbb"}
+            let f = filterName "aaa"
+            (map f  [x,y, y,x,y,x]) `shouldBe` [True,False, False, True, False, True]
+            filter f [x,y, y,x,y,x] `shouldBe` [x,x,x]
+
     
