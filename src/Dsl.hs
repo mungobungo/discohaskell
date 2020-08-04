c7cba97f19779f573cb3b9a1f4834c956066c725
oleg <oleg.taykalo@gmail.comgit config --global user.name oleg.taykalo>
2020-08-04 19:24:10 +0200

    added predicate with amount selectors

--- src/Dsl.hs
+++ src/Dsl.hs
@@ -20,18 +20,34 @@ defaultItem = Item {Dsl.id = "1", amount= 1, name ="PAP_100", lockedChildren =[]
 filterName :: String -> (Item -> Bool)
 filterName fname = \x -> (name x) == fname
 
-filterNameTree :: [String] -> (Item -> Bool)
--- filterNameTree [a,b]= \x -> 
---                                 let nm = (name x) == a
---                                     fn = filterName b
---                                 in
---                                     nm && ([] /= filter fn (freeChildren x))
-filterNameTree [a] = filterName a
+filterTreePredicate :: [String] -> (Item -> Bool) -> (Item->Bool)
+-- predicate is being used only on the lowest level
+filterTreePredicate [a] p = \x -> p x  && (filterName a) x
 
-filterNameTree [] = \x -> True
+filterTreePredicate [] p = \x -> True
 
-filterNameTree (a:rest) = \x -> 
+filterTreePredicate (a:rest) p = \x -> 
                                 let fnx = (filterName a) x
-                                    fnt = filterNameTree rest
+                                    fnt = filterTreePredicate rest p
                                 in
                                     fnx &&  ([] /= filter fnt (freeChildren x))
+
+
+filterNameTree :: [String] -> (Item -> Bool)
+filterNameTree items = filterTreePredicate items (\x -> True)
+
+
+filterTreeEqualAmount :: [String] -> Int -> (Item->Bool)
+filterTreeEqualAmount items limit = filterTreePredicate items (\x -> limit == Dsl.amount x)
+
+filterTreeLessOrEqualAmount :: [String] -> Int -> (Item->Bool)
+filterTreeLessOrEqualAmount items limit = filterTreePredicate items (\x ->  Dsl.amount x <= limit)
+
+filterTreeLessThanAmount :: [String] -> Int -> (Item->Bool)
+filterTreeLessThanAmount items limit = filterTreePredicate items (\x ->  Dsl.amount x < limit)
+
+filterTreeMoreOrEqualAmount :: [String] -> Int -> (Item->Bool)
+filterTreeMoreOrEqualAmount items limit = filterTreePredicate items (\x ->  Dsl.amount x >= limit)
+
+filterTreeMoreThanAmount :: [String] -> Int -> (Item->Bool)
+filterTreeMoreThanAmount items limit = filterTreePredicate items (\x -> Dsl.amount x > limit)
--- test/Spec.hs
+++ test/Spec.hs
@@ -19,15 +19,30 @@ main = hspec $ do
             let z = defaultItem {  name= "ccc", freeChildren = [x,x]}
             let b = defaultItem {  name = "ddd"}
             let c = defaultItem {  name= "ccc", freeChildren = [b,b]}
-            let deep = defaultItem {name = "deep", freeChildren = [c,z,y]}
-             
+            let deep = defaultItem {name = "deep", freeChildren = [c,z,y]}           
             filter (filterNameTree ["bbb","aaa"]) [y,y, z] `shouldBe` [y,y]
-            
             filter (filterNameTree ["ccc", "aaa"]) [z,y,y,c,b] `shouldBe` [z]
-
             filter (filterNameTree ["deep", "aaa"]) [deep] `shouldBe` []
-
             filter (filterNameTree ["deep", "ccc", "ddd"]) [deep] `shouldBe` [deep]
+        it "should work with yesno selector" $ do
+            let page1 = defaultItem {name = "extraPage", amount =15}
+            let page2 = defaultItem {name = "extraPage", amount =10}
+            let x= defaultItem{name = "photobook", freeChildren = [page1]}
+            
+            let y = defaultItem{name = "photobook", freeChildren = [page2]}
+
+            filter (filterTreeEqualAmount ["extraPage"] 10) [page2] `shouldBe` [page2]
+            filter (filterTreeEqualAmount ["extraPage"] 10) [page1] `shouldBe` []
+            filter (filterTreeEqualAmount ["extraPage"] 15) [page1] `shouldBe` [page1]
+            filter (filterTreeEqualAmount ["extraPage"] 10) [page1, page2] `shouldBe` [page2]
+
+            filter (filterTreeEqualAmount ["photobook", "extraPage"] 10) [x, y] `shouldBe` [y]
+            filter (filterTreeLessOrEqualAmount ["photobook", "extraPage"] 10) [x, y] `shouldBe` [y]
+            filter (filterTreeLessThanAmount ["photobook", "extraPage"] 11) [x, y] `shouldBe` [y]
+
+            filter (filterTreeMoreOrEqualAmount ["photobook", "extraPage"] 10) [x, y] `shouldBe` [x,y]
+            filter (filterTreeMoreThanAmount ["photobook", "extraPage"] 10) [x, y] `shouldBe` [x]
+
 
 
     
